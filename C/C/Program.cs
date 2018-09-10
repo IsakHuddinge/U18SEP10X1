c852e9a41207f7d1686ae6fbd9a219155e4d62fb
IsakHuddinge <isak.nywall@edu.huddinge.se>
2018-09-11 00:03:55 +0200

    completed C

--- C/C/Program.cs
+++ C/C/Program.cs
@@ -3,23 +3,22 @@ using System.Text.RegularExpressions;
 using System.Collections.Generic;
 using System.Linq;
 
-public class Words
+public class Unique_word
 {
-    public string Word {get; set; }
-    public int Count {get; set;}
+    public String Word  { get; set; }
+    public int Count { get; set; }
 }
 
-
-
-namespace C
+namespace D
 {
     class Program
     {
         static void Main(string[] args)
         {
-            string prompt = ">";    // prompt for when user input is required
+            //prompt for when user input is required
+            string prompt = ">";
             string user_input;
-            // wait until non-empty string is passed
+            //wait until non-empty string is passed
             while (true)
             {
                 Console.WriteLine("Write a sentence!");
@@ -39,20 +38,31 @@ namespace C
             // find all words in user_input
             MatchCollection regex_matches = regex_match_words.Matches(user_input);
 
-            var words_list = new List<Words>();
+            var word_list = new List<Unique_word> { };
 
-            foreach(Match match in regex_matches)
+            //add all words to list. if word exist's it adds 1 to counter
+            foreach (Match word in regex_matches)
             {
-                new Words() { }
+                string word_string = word.ToString();
+                if (word_list.Exists(x => x.Word == word_string))
+                {
+                    var index = word_list.FindIndex(x => x.Word == word_string);
+                    word_list[index].Count += 1;
+                }
+                else
+                {
+                    word_list.Add(new Unique_word { Word = word_string, Count = 1 });
+                }
+            }
+            //create new list with sorted order;
+            List<Unique_word> sorted_word_list= word_list.OrderByDescending(word => word.Count).ToList();
+
+            Console.WriteLine("Unique words sorted by occurence : ");
+            foreach (Unique_word word in sorted_word_list)
+            {
+                Console.WriteLine("{0} ({1})", word.Word, word.Count);
             }
 
-
-
-
-            for (int i = 0; i < index; i++)
-            {
-                Console.WriteLine("{0} ({1})", word_array[i], word_count_array[i]);
-            }
             Console.WriteLine("Press any key to continue. . .");
             Console.ReadKey();
         }

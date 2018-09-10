5bb830d37da359820fc1c0abe1299b6eee27e531
IsakHuddinge <isak.nywall@edu.huddinge.se>
2018-09-10 18:23:44 +0200

    gave up on c, finished e

---
+++ C/C/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Text.RegularExpressions;
+using System.Collections.Generic;
+using System.Linq;
+
+public class Words
+{
+    public string Word {get; set; }
+    public int Count {get; set;}
+}
+
+
+
+namespace C
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string prompt = ">";    // prompt for when user input is required
+            string user_input;
+            // wait until non-empty string is passed
+            while (true)
+            {
+                Console.WriteLine("Write a sentence!");
+                Console.Write(prompt);
+                user_input = Console.ReadLine();
+
+                if (user_input == "")
+                {
+                    Console.WriteLine("Don't leave it blank!");
+                }
+                else
+                {
+                    break;
+                }
+            }
+            Regex regex_match_words = new Regex(@"\w+");
+            // find all words in user_input
+            MatchCollection regex_matches = regex_match_words.Matches(user_input);
+
+            var words_list = new List<Words>();
+
+            foreach(Match match in regex_matches)
+            {
+                new Words() { }
+            }
+
+
+
+
+            for (int i = 0; i < index; i++)
+            {
+                Console.WriteLine("{0} ({1})", word_array[i], word_count_array[i]);
+            }
+            Console.WriteLine("Press any key to continue. . .");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ E/E/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace E
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string prompt = ">";    //prompt for when user input is required
+            string user_input;
+            string sentence = "";
+            while (true)
+            {
+                //wait until non-empty string is passed
+                while (true)
+                {
+                    Console.WriteLine("Type a word! Type \"AVSLUTA\" to stop");
+                    Console.Write(prompt);
+                    user_input = Console.ReadLine();
+
+                    if (user_input == "")
+                    {
+                        Console.WriteLine("Don't leave it blank!");
+                    }
+                    else
+                    {
+                        break;
+                    }
+                }
+
+                if (user_input.ToLower() == "avsluta")
+                {
+                    break;
+                }
+                sentence += " " + user_input.Trim();
+                
+            }
+
+            Console.WriteLine("Your sentence is now:{0}.",sentence);
+            Console.WriteLine("Press any key to continue. . .");
+            Console.ReadKey();
+
+        }
+    }
+}
